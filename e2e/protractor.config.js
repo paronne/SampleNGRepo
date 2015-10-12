8e076375374e510b0df7c3f1883c2cc2079b0d9d
Anastasija <efistasija@gmail.com>
2015-10-12 09:57:11 +0200

    Modified parameters for new project setting

--- e2e/protractor.config.js
+++ e2e/protractor.config.js
@@ -124,7 +124,7 @@ exports.config = {
   //
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
-  baseUrl: 'http://sampleng.eu-gb.mybluemix.net/',
+  baseUrl: 'http://samplengapp-dev.eu-gb.mybluemix.net/',
 
   // Selector for the element housing the angular app - this defaults to
   // body, but is necessary if ng-app is on a descendant of <body>
