0bfdde7ba565ecdd50536e08e2bff2e6a38c7ed9
paronne <paronne@servoy.com>
2015-10-21 14:39:26 +0200

    Protractor tests on component

--- e2e/spec/svyAngularUIBootstrapButtonsRadio_test/test_buttons_radio_onDataChange_spec.js
+++ e2e/spec/svyAngularUIBootstrapButtonsRadio_test/test_buttons_radio_onDataChange_spec.js
@@ -6,19 +6,21 @@ describe('test_buttons_radio', function() {
 	
     value = element(by.xpath("//div[@data-svy-name='buttonsRadio']/div/div[3]/input")).getAttribute('value');
     expect(value).toContain("");
-    bool = isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[contains(@class,'active')]"));
+    
+	bool = browser.isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[contains(@class,'active')]"));
     expect(bool).toBe(false);
     element(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[2]")).click();
     // TODO: waitFor: {expression}
     element(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[3]")).click();
-    bool = isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[2][contains(@class,'active')]"));
+    bool = browser.isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[2][contains(@class,'active')]"));
     expect(bool).toBe(false);
     // TODO: waitFor: {expression}
     element(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label")).click();
     // TODO: waitFor: {expression}
-    navigate.refresh();
-    bool = isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[3][contains(@class,'active')]"));
-      expect(bool).toBe(true);
+	browser.refresh();
+	bool = browser.isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[3][contains(@class,'active')]"));
+    expect(bool).toBe(true);
+	
     element(by.xpath("//div[@data-svy-name='buttonsRadio']/div/div[3]/input")).sendKeys("2");
     element(by.xpath("//div[@data-svy-name='buttonsRadio']/div/div[3]/input")).sendKeys("0");
     element(by.xpath("//div[@data-svy-name='buttonsRadio']/div/div[3]/input")).sendKeys("1");
