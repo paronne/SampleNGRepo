3a71d50175ec1e154aeda0dbd4cd6f8d53ead471
paronne <paronne@servoy.com>
2015-10-21 09:18:28 +0200

    fix issue in test

--- e2e/spec/svyAngularUIBootstrapButtonsRadio_test/test_buttons_radio_onDataChange_spec.js
+++ e2e/spec/svyAngularUIBootstrapButtonsRadio_test/test_buttons_radio_onDataChange_spec.js
@@ -5,7 +5,7 @@ describe('test_buttons_radio', function() {
     browser.get("http://localhost:8080/solutions/svyAngularUIBootstrapButtonsRadio_test/index.html?f=buttonsRadio");
 	
     value = element(by.xpath("//div[@data-svy-name='buttonsRadio']/div/div[3]/input")).getAttribute('value');
-    expect(value).toContain("" + );
+    expect(value).toContain("");
     bool = isElementPresent(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[contains(@class,'active')]"));
     expect(bool).toBe(false);
     element(by.xpath("//data-angularuibootstrap-buttons-radio[@data-svy-name='buttonsRadio.buttons_radio_1']/div/label[2]")).click();
