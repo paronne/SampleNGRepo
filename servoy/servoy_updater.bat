02179dc3f8e74fdc18ed5a4a7f4ac060ac37d735
paronne <paronne@servoy.com>
2016-01-27 17:24:52 +0100

    replace install

--- servoy/servoy_updater.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem To enable logging change 'java' into 'java -DSTACKTRACE=true'
-java -Djava.awt.headless=true -Xmx256m -Xms64m -XX:MaxPermSize=128m -jar servoy_updater.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
-pause
-
